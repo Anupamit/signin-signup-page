e23f3933ea3f4394aca48fcafc4d5a5cb2a48627
Anupam kumar <anupamit011@gmail.com>
2022-06-07 13:06:06 +0530

    all the file are impported here

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './Home';
+import Profile from './Profile';
+import Signin from './Signin';
+import Signup from './Signup';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="signin" element={<Signin />} />
+              <Route path="signup" element={<Signup />} />
+              <Route path="profile" element={<Profile />} />
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
